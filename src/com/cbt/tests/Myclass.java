1d07a4e736caec8c500a0aac51a5454cb96248f4
mercik <muhittinercik@gmail.com>
2020-03-07 01:13:52 -0600

    MyFirstCommit

---
+++ src/com/cbt/tests/Myclass.java
@@ -0,0 +1,11 @@
+package com.cbt.tests;
+
+public class Myclass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        String std = "Java is Fun!";
+        System.out.println(std);
+    }
+}
